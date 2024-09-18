a82ae806072c11d25424bfece78fde8aea6bbd5e
Ninhtran949 <118685759+Ninhtran949@users.noreply.github.com>
2024-09-18 16:00:11 +0700

    ok10

---
+++ app/src/main/java/com/example/Sachpee/Service/MongoDBConnection.java
@@ -0,0 +1,12 @@
+package com.example.Sachpee.Service;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+public class MongoDBConnection {
+    private static final String CONNECTION_STRING = "mongodb+srv:/root:Loc220897@cluster0.kligl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+    public static MongoClient getMongoClient() {
+        return MongoClients.create(CONNECTION_STRING);
+    }
+}
